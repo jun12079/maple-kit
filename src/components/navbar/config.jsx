aa9d9eae866709fa53ecbc1727eeb73e24d2923f
Jun <jeffery851007@gmail.com>
2025-10-29 09:59:04 +0800

    feat: added HEXA skill progress calculator
    - 新增 HEXA進度計算功能
    - 新增 HEXA技能需求表格

--- src/components/navbar/config.jsx
+++ src/components/navbar/config.jsx
@@ -1,6 +1,7 @@
 import Logo from "@/assets/images/Destiny_Ritual_Fan_icon.png";
 import DestinyRitualFanIcon from "@/assets/images/Destiny_Ritual_Fan_icon.png";
 import GenesisRitualFanIcon from "@/assets/images/Genesis_Ritual_Fan_icon.png";
+import SolErdaFragmentIcon from  "@/assets/images/items/icons/SolErdaFragment_icon.png";
 import ArcaneAuthenticSymbolSelectCoupon from  "@/assets/images/symbol/Arcane_Authentic_Symbol_Select_Coupon.png";
 import baldrixIcon from '@/assets/images/bosses/icons/Baldrix_icon.png';
 
@@ -26,6 +27,11 @@ export const menu = [
         title: "創世武器",
         icon: GenesisRitualFanIcon,
         url: "/calc/genesis-weapon",
+      },
+      {
+        title: "HEXA技能",
+        icon: SolErdaFragmentIcon,
+        url: "/calc/hexa-skill",
       },
     ],
   },
